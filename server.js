dab5fadc783c19b621b772f853ae7a9cfc07bed1
pchessah <paulchesa1@gmail.com>
2020-02-29 15:58:31 +0300

    heroku posting

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require ("express")
 const mongoose = require("mongoose")
 const router = require("./routes/index")
+const path = require("path")
+const PORT =  process.env.PORT || 5000
+require("dotenv").config
+const LOCAL_DB = "mongodb://127.0.0.1:27017/articles"
+
 
 const app =  express()
-const PORT = 5000
-const MONGO_URI = "mongodb://localhost:27017/articles"
+
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use("/api", router)
 
-mongoose.connect(MONGO_URI, {useNewUrlParser: true, useFindAndModify: false})
+mongoose.connect(process.env.MONGODB_URI || LOCAL_DB, {useNewUrlParser: true, useFindAndModify: false})
 
 mongoose.connection.once("open", ()=>{
     console.log("Connected to database");
@@ -19,6 +23,15 @@ mongoose.connection.once("open", ()=>{
 mongoose.connection.on("error", (error) =>{
     console.log("Mongoose connection error: "+ error);
 })
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"))
+
+    app.get("*", (req, res)=>{
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    })
+}
+
 
 app.listen(PORT, ()=>{
     console.log(`Server is listening on port ${PORT}`);
